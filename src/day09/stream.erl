ae9d52aff5eef5b2b749d09dd9f10945e315dcaa
Maurits Out <17969131+maurits-out@users.noreply.github.com>
2020-10-17 12:14:41 +0200

    Day 9, part 2.

--- src/day09/stream.erl
+++ src/day09/stream.erl
@@ -1,17 +1,23 @@
 -module(stream).
--export([part1/0]).
+-export([part1/0, part2/0]).
 
 read_stream() ->
   {ok, File} = file:read_file("input.txt"),
   string:trim(unicode:characters_to_list(File)).
 
-total_score([], _, _, Score) -> Score;
-total_score([${ | T], Depth, false, Score) -> total_score(T, Depth + 1, false, Score);
-total_score([$} | T], Depth, false, Score) -> total_score(T, Depth - 1, false, Score + Depth);
-total_score([$< | T], Depth, false, Score) -> total_score(T, Depth, true, Score);
-total_score([$> | T], Depth, true, Score) -> total_score(T, Depth, false, Score);
-total_score([$!, _ | T], Depth, true, Score) -> total_score(T, Depth, true, Score);
-total_score([_ | T], Depth, Garbage, Score) -> total_score(T, Depth, Garbage, Score).
+process([], _, _, Score, GarbageCount) -> {Score, GarbageCount};
+process([${ | T], Depth, false, Score, GarbageCount) -> process(T, Depth + 1, false, Score, GarbageCount);
+process([$} | T], Depth, false, Score, GarbageCount) -> process(T, Depth - 1, false, Score + Depth, GarbageCount);
+process([$< | T], Depth, false, Score, GarbageCount) -> process(T, Depth, true, Score, GarbageCount);
+process([$> | T], Depth, true, Score, GarbageCount) -> process(T, Depth, false, Score, GarbageCount);
+process([$!, _ | T], Depth, true, Score, GarbageCount) -> process(T, Depth, true, Score, GarbageCount);
+process([_ | T], Depth, false, Score, GarbageCount) -> process(T, Depth, false, Score, GarbageCount);
+process([_ | T], Depth, true, Score, GarbageCount) -> process(T, Depth, true, Score, GarbageCount + 1).
 
 part1() ->
-  total_score(read_stream(), 0, false, 0).
+  {TotalScore, _} = process(read_stream(), 0, false, 0, 0),
+  TotalScore.
+
+part2() ->
+  {_, GarbageCount} = process(read_stream(), 0, false, 0, 0),
+  GarbageCount.
