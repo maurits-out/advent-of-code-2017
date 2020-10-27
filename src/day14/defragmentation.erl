d0f5ad38c750371ef8a3f747b8d07ce3a25c889a
maurits-out <maurits.out@users.noreply.github.com>
2020-10-27 21:10:32 +0100

    Day 14, part 2.

--- src/day14/defragmentation.erl
+++ src/day14/defragmentation.erl
@@ -1,5 +1,5 @@
 -module(defragmentation).
--export([part1/0]).
+-export([part1/0, part2/0]).
 -define(PUZZLE_INPUT, "oundnydw").
 -define(GRID_SIZE, 128).
 
@@ -23,18 +23,50 @@ create_bit_mapping() ->
     $f => "1111"
   }.
 
-create_row(Sequence, BitMapping) ->
+create_grid_row(Sequence, BitMapping) ->
   HashInput = ?PUZZLE_INPUT ++ "-" ++ integer_to_list(Sequence),
   Hash = knot_hash:knot(HashInput),
   lists:flatten([maps:get(Char, BitMapping) || Char <- Hash]).
 
 create_grid() ->
   BitMapping = create_bit_mapping(),
-  [create_row(S, BitMapping) || S <- lists:seq(0, ?GRID_SIZE - 1)].
+  [create_grid_row(S, BitMapping) || S <- lists:seq(0, ?GRID_SIZE - 1)].
 
-count_squares_used() ->
-  Grid = create_grid(),
-  length(lists:filter(fun(Digit) -> Digit == $1 end, lists:flatten(Grid))).
+grid_row_to_coordinates([], _, _, Acc) ->
+  Acc;
+grid_row_to_coordinates([$1 | T], RowNum, ColNum, Acc) ->
+  grid_row_to_coordinates(T, RowNum, ColNum + 1, [{RowNum, ColNum} | Acc]);
+grid_row_to_coordinates([$0 | T], RowNumber, ColumnNumber, Acc) ->
+  grid_row_to_coordinates(T, RowNumber, ColumnNumber + 1, Acc).
+
+grid_to_coordinates([], _, Acc) ->
+  Acc;
+grid_to_coordinates([Row | Rest], RowNumber, Acc) ->
+  NewAcc = grid_row_to_coordinates(Row, RowNumber, 0, []) ++ Acc,
+  grid_to_coordinates(Rest, RowNumber + 1, NewAcc).
+
+adjacent_new_used_squares({Row, Column}, UsedSquares, Visited) ->
+  Adjacent = [{Row - 1, Column}, {Row, Column + 1}, {Row + 1, Column}, {Row, Column - 1}],
+  [Square || Square <- Adjacent, sets:is_element(Square, UsedSquares), not sets:is_element(Square, Visited)].
+
+find_used_squares_in_region([], _, Visited) ->
+  Visited;
+find_used_squares_in_region([Current | Remaining], UsedSquares, Visited) ->
+  AdjacentUsedSquares = adjacent_new_used_squares(Current, UsedSquares, Visited),
+  find_used_squares_in_region(Remaining ++ AdjacentUsedSquares, UsedSquares, sets:add_element(Current, Visited)).
+
+count_regions([], Count) ->
+  Count;
+count_regions([Square | T], Count) ->
+  UsedSquares = sets:from_list([Square | T]),
+  SquaresInRegion = find_used_squares_in_region([Square], UsedSquares, sets:new()),
+  Remaining = sets:subtract(UsedSquares, SquaresInRegion),
+  count_regions(sets:to_list(Remaining), Count + 1).
 
 part1() ->
-  count_squares_used().
+  UsedSquares = grid_to_coordinates(create_grid(), 0, []),
+  length(UsedSquares).
+
+part2() ->
+  UsedSquares = grid_to_coordinates(create_grid(), 0, []),
+  count_regions(UsedSquares, 0).
