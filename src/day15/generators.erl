b87836597366fb37970e9ba82ee4bf16069b5352
maurits-out <maurits.out@users.noreply.github.com>
2020-10-29 12:55:04 +0100

    Day 15.

--- src/day15/generators.erl
+++ src/day15/generators.erl
@@ -1,18 +1,27 @@
 -module(generators).
--export([part1/0]).
--define(FACTOR_A, 16807).
--define(FACTOR_B, 48271).
--define(ITERATIONS, 40000000).
+-export([part1/0, part2/0]).
+
+part1() ->
+  count_matching_pairs(create_generator(16807), create_generator(48271), 40000000).
+
+part2() ->
+  count_matching_pairs(create_generator(16807, 4), create_generator(48271, 8), 5000000).
 
 puzzle_input() ->
   {634, 301}.
 
-part1() ->
-  {StartA, StartB} = puzzle_input(),
-  count_matching_pairs(StartA, StartB, 0, 0).
+create_generator(Factor) ->
+  fun(Value) -> (Value * Factor) rem 2147483647 end.
 
-next(Value, Factor) ->
-  (Value * Factor) rem 2147483647.
+create_generator(Factor, MultipleOf) ->
+  GeneratorPart1 = create_generator(Factor),
+  fun Generator(Value) ->
+    NextValue = GeneratorPart1(Value),
+    case NextValue rem MultipleOf of
+      0 -> NextValue;
+      _ -> Generator(NextValue)
+    end
+  end.
 
 match(A, B) ->
   case (A band 16#ffff) == (B band 16#ffff) of
@@ -20,9 +29,13 @@ match(A, B) ->
     false -> 0
   end.
 
-count_matching_pairs(_, _, ?ITERATIONS, Acc) ->
+count_matching_pairs(GeneratorA, GeneratorB, Iterations) ->
+  {StartA, StartB} = puzzle_input(),
+  count_matching_pairs(StartA, StartB, GeneratorA, GeneratorB, Iterations, 0).
+
+count_matching_pairs(_, _, _, _, 0, Acc) ->
   Acc;
-count_matching_pairs(A, B, Count, Acc) ->
-  NextA = next(A, ?FACTOR_A),
-  NextB = next(B, ?FACTOR_B),
-  count_matching_pairs(NextA, NextB, Count + 1, Acc + match(NextA, NextB)).
+count_matching_pairs(A, B, GeneratorA, GeneratorB, Count, Acc) ->
+  NextA = GeneratorA(A),
+  NextB = GeneratorB(B),
+  count_matching_pairs(NextA, NextB, GeneratorA, GeneratorB, Count - 1, Acc + match(NextA, NextB)).
