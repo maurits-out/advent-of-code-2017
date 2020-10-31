b5b866a9be21053b6f42ed2238ccdefa07427d06
Maurits Out <17969131+maurits-out@users.noreply.github.com>
2020-10-31 21:06:37 +0100

    Day 17

--- src/day17/spinlock.erl
+++ src/day17/spinlock.erl
@@ -1,16 +1,17 @@
 -module(spinlock).
 -export([part1/0, part2/0]).
--define(ITERATIONS, 2017).
 -define(PUZZLE_INPUT, 356).
+-define(LAST_VALUE_PART_1, 2017).
+-define(LAST_VALUE_PART_2, 50000000).
 
-spinlock_part1(Buffer, Position, 2018) ->
+spinlock_part1(Buffer, Position, ?LAST_VALUE_PART_1 + 1) ->
   lists:nth(Position + 2, Buffer);
 spinlock_part1(Buffer, Position, NextValue) ->
   NextPosition = (Position + ?PUZZLE_INPUT) rem length(Buffer),
   UpdatedBuffer = lists:sublist(Buffer, NextPosition + 1) ++ [NextValue | lists:nthtail(NextPosition + 1, Buffer)],
   spinlock_part1(UpdatedBuffer, NextPosition + 1, NextValue + 1).
 
-spinlock_part2(ValueAfter0, _, 50000001) ->
+spinlock_part2(ValueAfter0, _, ?LAST_VALUE_PART_2 + 1) ->
   ValueAfter0;
 spinlock_part2(ValueAfter0, Position, NextValue) ->
   case (Position + ?PUZZLE_INPUT) rem NextValue of
@@ -18,8 +19,6 @@ spinlock_part2(ValueAfter0, Position, NextValue) ->
     NextPosition -> spinlock_part2(ValueAfter0, NextPosition + 1, NextValue + 1)
   end.
 
-part1() ->
-  spinlock_part1([0], 0, 1).
+part1() -> spinlock_part1([0], 0, 1).
 
-part2() ->
-  spinlock_part2(null, 0, 1).
+part2() -> spinlock_part2(null, 0, 1).
