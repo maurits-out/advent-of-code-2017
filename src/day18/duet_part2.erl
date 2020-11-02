2fa6feaaef8ab5bc4ac7464bf1c58e577d5d6163
Maurits Out <17969131+maurits-out@users.noreply.github.com>
2020-11-02 20:45:51 +0100

    Day 18 part 2.

--- src/day18/duet_part2.erl
+++ src/day18/duet_part2.erl
@@ -1,6 +1,8 @@
 -module(duet_part2).
 -export([run/0]).
 
+-record(state, {id, pc = 0, registers, partner_pid, num_values_sent = 0}).
+
 read_instructions() ->
   {ok, Bin} = file:read_file("input.txt"),
   Bins = binary:split(Bin, <<$\n>>, [global]),
@@ -11,7 +13,7 @@ parse_line(Line) ->
   [Instruction | Args] = string:lexemes(Line, " "),
   {list_to_atom(Instruction), Args}.
 
-register_value(Expression, Registers) ->
+value_of_register(Expression, Registers) ->
   Register = lists:nth(1, Expression),
   maps:get(Register, Registers, 0).
 
@@ -21,49 +23,51 @@ update_register(Expression, Value, Registers) ->
 
 evaluate(Expression, Registers) ->
   case string:to_integer(Expression) of
-    {error, _} -> register_value(Expression, Registers);
+    {error, _} -> value_of_register(Expression, Registers);
     {Value, _} -> Value
   end.
 
-execute(ProgramID, Instructions, PC, Registers, PartnerProgramPid, NumValuesSent) ->
-  case array:get(PC, Instructions) of
+execute(Instructions, #state{pc = PC, registers = Registers} = State) ->
+  case array:get(State#state.pc, Instructions) of
     {snd, [X]} ->
-      PartnerProgramPid ! evaluate(X, Registers),
-      execute(ProgramID, Instructions, PC + 1, Registers, PartnerProgramPid, NumValuesSent + 1);
+      State#state.partner_pid ! evaluate(X, Registers),
+      execute(Instructions, State#state{pc = PC + 1, num_values_sent = State#state.num_values_sent + 1});
     {set, [X, Y]} ->
       Value = evaluate(Y, Registers),
-      execute(ProgramID, Instructions, PC + 1, update_register(X, Value, Registers), PartnerProgramPid, NumValuesSent);
+      execute(Instructions, State#state{pc = PC + 1, registers = update_register(X, Value, Registers)});
     {add, [X, Y]} ->
-      Value = register_value(X, Registers) + evaluate(Y, Registers),
-      execute(ProgramID, Instructions, PC + 1, update_register(X, Value, Registers), PartnerProgramPid, NumValuesSent);
+      Value = value_of_register(X, Registers) + evaluate(Y, Registers),
+      execute(Instructions, State#state{pc = PC + 1, registers = update_register(X, Value, Registers)});
     {mul, [X, Y]} ->
-      Value = register_value(X, Registers) * evaluate(Y, Registers),
-      execute(ProgramID, Instructions, PC + 1, update_register(X, Value, Registers), PartnerProgramPid, NumValuesSent);
+      Value = value_of_register(X, Registers) * evaluate(Y, Registers),
+      execute(Instructions, State#state{pc = PC + 1, registers = update_register(X, Value, Registers)});
     {mod, [X, Y]} ->
-      Value = register_value(X, Registers) rem evaluate(Y, Registers),
-      execute(ProgramID, Instructions, PC + 1, update_register(X, Value, Registers), PartnerProgramPid, NumValuesSent);
+      Value = value_of_register(X, Registers) rem evaluate(Y, Registers),
+      execute(Instructions, State#state{pc = PC + 1, registers = update_register(X, Value, Registers)});
     {rcv, [X]} ->
       receive
         Value ->
-          execute(ProgramID, Instructions, PC + 1, update_register(X, Value, Registers), PartnerProgramPid, NumValuesSent)
+          execute(Instructions, State#state{pc = PC + 1, registers = update_register(X, Value, Registers)})
       after 100 ->
-        io:format("Program with ID ~p sent ~p values~n", [ProgramID, NumValuesSent])
+        io:format("Program with ID ~p sent ~p values~n", [State#state.id, State#state.num_values_sent])
       end;
     {jgz, [X, Y]} ->
       Offset = case evaluate(X, Registers) of
                  Value when Value > 0 -> evaluate(Y, Registers);
                  _ -> 1
                end,
-      execute(ProgramID, Instructions, PC + Offset, Registers, PartnerProgramPid, NumValuesSent)
+      execute(Instructions, State#state{pc = PC + Offset})
   end.
 
 start(ProgramID, Instructions) ->
   PartnerProgramPid = receive Pid -> Pid end,
-  execute(ProgramID, Instructions, 0, #{$p => ProgramID}, PartnerProgramPid, 0).
+  State = #state{id = ProgramID, registers = #{$p => ProgramID}, partner_pid = PartnerProgramPid},
+  execute(Instructions, State).
 
 run() ->
   Instructions = read_instructions(),
   Pid0 = spawn(fun() -> start(0, Instructions) end),
   Pid1 = spawn(fun() -> start(1, Instructions) end),
   Pid0 ! Pid1,
-  Pid1 ! Pid0.
+  Pid1 ! Pid0,
+  ok.
