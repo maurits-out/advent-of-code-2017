b244abe1c74f7b087944a3435a1bee6e9a22660e
Maurits Out <17969131+maurits-out@users.noreply.github.com>
2020-11-07 10:55:22 +0100

    Day 20.

--- src/day20/particles.erl
+++ src/day20/particles.erl
@@ -1,7 +1,8 @@
 -module(particles).
--export([part1/0]).
+-export([part1/0, part2/0]).
 -record(vector, {x, y, z}).
 -record(particle, {position, velocity, acceleration}).
+-define(ITERATIONS, 50).
 
 read_lines() ->
   {ok, Bin} = file:read_file("src/day20/input.txt"),
@@ -36,11 +37,24 @@ update_particle(#particle{position = Position, velocity = Velocity, acceleration
   NewPosition = add(Position, NewVelocity),
   Particle#particle{position = NewPosition, velocity = NewVelocity}.
 
+group_by_position([], Groups) ->
+  Groups;
+group_by_position([Particle | T], Groups) ->
+  #particle{position = Position} = Particle,
+  Values = maps:get(Position, Groups, []),
+  group_by_position(T, maps:put(Position, [Particle | Values], Groups)).
+
+remove_collided_particles(Particles) ->
+  Grouped = group_by_position(Particles, maps:new()),
+  FilteredByGroupSize = maps:filter(fun (_, Group) -> length(Group) == 1 end, Grouped),
+  [Particle || {_, [Particle]} <- maps:to_list(FilteredByGroupSize)].
+
 update_particles(Particles) ->
-  [update_particle(P) || P <- Particles].
+  UpdatedParticles = [update_particle(P) || P <- Particles],
+  remove_collided_particles(UpdatedParticles).
 
 iterate(Particles, Iterations) ->
-  lists:foldl(fun(_, P) -> update_particles(P) end, Particles, lists:seq(1, Iterations)).
+  lists:foldl(fun(_, Particle) -> update_particles(Particle) end, Particles, lists:seq(1, Iterations)).
 
 compare_distance(#vector{x = X1, y = Y1, z = Z1}, #vector{x = X2, y = Y2, z = Z2}) ->
   abs(X1) =< abs(X2) andalso abs(Y1) =< abs(Y2) andalso abs(Z1) =< abs(Z2).
@@ -53,6 +67,9 @@ find_particle_with_smallest_acceleration(Particles) ->
   ID.
 
 part1() ->
-  Particles = parse_input(),
-  ID = find_particle_with_smallest_acceleration(Particles),
+  ID = find_particle_with_smallest_acceleration(parse_input()),
   io:format("Particle closest to origin (part 1): ~p~n", [ID]).
+
+part2() ->
+  Remaining = iterate(parse_input(), ?ITERATIONS),
+  io:format("Number of particles after ~p iterations (part 2): ~p~n", [?ITERATIONS, length(Remaining)]).
