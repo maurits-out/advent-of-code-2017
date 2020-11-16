2aeabba1a0a51a5627775e227b2b47c06cdb9fc8
Maurits Out <17969131+maurits-out@users.noreply.github.com>
2020-11-16 21:45:12 +0100

    Day 24, part 2.

--- src/day24/bridge.erl
+++ src/day24/bridge.erl
@@ -2,7 +2,7 @@
 -export([part1/0, part2/0]).
 
 read_input() ->
-  {ok, Bin} = file:read_file("src/day24/input.txt"),
+  {ok, Bin} = file:read_file("input.txt"),
   Bins = binary:split(Bin, <<$\n>>, [global]),
   [binary_to_list(B) || B <- Bins, byte_size(B) > 0].
 
@@ -22,44 +22,38 @@ find_matching_components(Pin, Components) ->
 next_pin(CurrentPin, {CurrentPin, OtherPin}) -> OtherPin;
 next_pin(CurrentPin, {OtherPin, CurrentPin}) -> OtherPin.
 
-strength_of_strongest_bridge(CurrentPin, Remaining, Bridge) ->
-  case find_matching_components(CurrentPin, Remaining) of
-    [] ->
-      strength(Bridge);
-    Matching ->
-      lists:max([extend_strongest_bridge(CurrentPin, C, Remaining, Bridge) || C <- Matching])
-  end.
-
-extend_strongest_bridge(CurrentPin, NewComponent, Remaining, Bridge) ->
-  NextPin = next_pin(CurrentPin, NewComponent),
-  NewRemaining = sets:del_element(NewComponent, Remaining),
-  strength_of_strongest_bridge(NextPin, NewRemaining, [NewComponent | Bridge]).
-
-compare(Bridge1, Bridge2) ->
-  length(Bridge1) > length(Bridge2) orelse
-    (length(Bridge1) == length(Bridge2) andalso strength(Bridge1) >= strength(Bridge2)).
-
-select_longest(Bridges) ->
-  Sorted = lists:sort(fun compare/2, Bridges),
-  lists:nth(1, Sorted).
-
-longest_bridge(CurrentPin, Remaining, Bridge) ->
+find_bridge(Select, CurrentPin, Remaining, Bridge) ->
   case find_matching_components(CurrentPin, Remaining) of
     [] ->
       Bridge;
     Matching ->
-      select_longest([extend_longest_bridge(CurrentPin, C, Remaining, Bridge) || C <- Matching])
+      Select([extend_bridge(Select, CurrentPin, C, Remaining, Bridge) || C <- Matching])
   end.
 
-extend_longest_bridge(CurrentPin, NewComponent, Remaining, Bridge) ->
+extend_bridge(Select, CurrentPin, NewComponent, Remaining, Bridge) ->
   NextPin = next_pin(CurrentPin, NewComponent),
   NewRemaining = sets:del_element(NewComponent, Remaining),
-  longest_bridge(NextPin, NewRemaining, [NewComponent | Bridge]).
+  find_bridge(Select, NextPin, NewRemaining, [NewComponent | Bridge]).
+
+compare_strength(Bridge1, Bridge2) ->
+  strength(Bridge1) >= strength(Bridge2).
+
+select_strongest(Bridges) ->
+  Sorted = lists:sort(fun compare_strength/2, Bridges),
+  lists:nth(1, Sorted).
+
+compare_length(Bridge1, Bridge2) ->
+  length(Bridge1) > length(Bridge2) orelse
+    (length(Bridge1) == length(Bridge2) andalso strength(Bridge1) >= strength(Bridge2)).
+
+select_longest(Bridges) ->
+  Sorted = lists:sort(fun compare_length/2, Bridges),
+  lists:nth(1, Sorted).
 
 part1() ->
-  Strength = strength_of_strongest_bridge(0, read_components(), []),
+  Strength = strength(find_bridge(fun select_strongest/1, 0, read_components(), [])),
   io:format("Strength of the strongest bridge: ~p~n", [Strength]).
 
 part2() ->
-  Strength = strength(longest_bridge(0, read_components(), [])),
+  Strength = strength(find_bridge(fun select_longest/1, 0, read_components(), [])),
   io:format("Strength of the longest bridge: ~p~n", [Strength]).
